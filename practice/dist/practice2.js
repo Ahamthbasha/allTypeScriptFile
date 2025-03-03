5b81a529d1d503df0ac63f6f679a4033bf400e36
ahamathbasha <bashachristopherallah786@gmail.com>
2025-03-04 05:01:01 +0530

    typescript practice 2

--- practice/dist/practice2.js
+++ practice/dist/practice2.js
@@ -57,3 +57,58 @@ let userName = "abcd";
 //         console.log(error)
 //     }
 // }
+// type details={
+//     userId:number,
+//     id:number,
+//     title:string,
+//     body:string
+// }
+// async function fetchUser():Promise<details[]>{
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.com/posts')
+//         const data:details[]=await response.json()
+//         return data
+//     } catch (error) {
+//         return []
+//     }
+// }
+// fetchUser()
+// .then((response)=>{
+//     console.log(response)
+// })
+// .catch((reject)=>{
+//     console.log(reject)
+// })
+// let array:[string,...number[],boolean]=["basha",1,2,3,4,4,5,true]
+// async function add(a:number,b:number):Promise<number>{
+//     return new Promise((resolve,reject)=>{
+//         if(a && b){
+//             resolve(a+b)
+//         }else{
+//             reject ("not allowed")
+//         }
+//     })
+// }
+// add(5,4)
+// .then((resolve)=>{
+//     console.log(resolve)
+// })
+// .catch((reject)=>{
+//     console.log(reject)
+// })
+// function add(a:number,b:number):Promise<number>{
+//     return new Promise((resolve,reject)=>{
+//         if(a && b){
+//             resolve(a+b)
+//         }else{
+//             reject("add not allowed")
+//         }
+//     })
+// }
+// add(5,5)
+// .then((resolve)=>{
+//     console.log(resolve)
+// })
+// .catch((reject)=>{
+//     console.log(reject)
+// })
